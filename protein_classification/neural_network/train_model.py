0478de810b7594cdc7f7fbc07605e1a745623808
Ognjen Neskovic <oneskovic0@gmail.com>
2022-04-01 12:24:36 +0200

    Update requirements and neural net training

--- protein_classification/neural_network/train_model.py
+++ protein_classification/neural_network/train_model.py
@@ -102,32 +102,29 @@ def objective(trial):
     data = pd.read_csv(data_path)
     # Preprocess data
     x, y = preprocess_encode_ngram(data)
+    x = torch.tensor(x, dtype=torch.float32)
+    y = torch.tensor(y, dtype=torch.long)
     # Split data
     x_train, x_rem, y_train, y_rem = train_test_split(x, y, test_size=0.2, random_state=42)
     x_opt, x_val, y_opt, y_val = train_test_split(x_rem, y_rem, test_size=0.5, random_state=42)
     # Convert to torch tensors
-    x_train = torch.tensor(x_train, dtype=torch.float32)
-    x_val = torch.tensor(x_val, dtype=torch.float32)
-    x_opt = torch.tensor(x_opt, dtype=torch.float32)
-
-    y_train = torch.tensor(y_train, dtype=torch.long)
-    y_val = torch.tensor(y_val, dtype=torch.long)
-    y_opt = torch.tensor(y_opt, dtype=torch.long)
 
     hparams = {'neuron_cnt1': neuron_cnt1, 'neuron_cnt2': neuron_cnt2, 'learning_rate': learning_rate}
     model = create_model(hparams, x.shape[1])
     optimze_acc = train_model(model, x_train, x_val, y_train, y_val, hparams, trial, True)
     return optimze_acc
 
-data = pd.read_csv(data_path)    
-x, y = preprocess_encode_ngram(data)
-study = optuna.create_study(pruner=optuna.pruners.HyperbandPruner(), direction='maximize')
-study.optimize(objective, n_trials=10)
 
-model = create_model(study.best_params, x.shape[1])
-val_loss, val_acc = validate(study.best_params, model)
-print('Validation loss: ', val_loss)
-print('Validation accuracy: ', val_acc)
+def main():
+    data = pd.read_csv(data_path)    
+    x, y = preprocess_encode_ngram(data)
+    study = optuna.create_study(pruner=optuna.pruners.HyperbandPruner(), direction='maximize')
+    study.optimize(objective, n_trials=10)
 
-pickle.dump(model, open('best_model.pkl', 'wb+'))
-pickle.dump(study, open('study.pkl', 'wb+'))
+    model = create_model(study.best_params, x.shape[1])
+    val_loss, val_acc = validate(study.best_params, model)
+    print('Validation loss: ', val_loss)
+    print('Validation accuracy: ', val_acc)
+
+    pickle.dump(model, open('best_model.pkl', 'wb+'))
+    pickle.dump(study, open('study.pkl', 'wb+'))
